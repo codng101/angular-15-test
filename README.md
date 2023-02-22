b48b19002a9d4e2643f8df1b5a95506de845d2a8
codng101 <33670394+codng101@users.noreply.github.com>
2023-02-22 14:38:03 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-15-test
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-xfhqxe)
---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import 'zone.js/dist/zone';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { bootstrapApplication } from '@angular/platform-browser';
+
+@Component({
+  selector: 'my-app',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <h1>Hello from {{name}}!</h1>
+    <a target="_blank" href="https://angular.io/start">
+      Learn more about Angular 
+    </a>
+  `,
+})
+export class App {
+  name = 'Angular';
+  private static string1: string = 'value';
+  private static string2 = new String(App.string1);
+}
+
+bootstrapApplication(App);
